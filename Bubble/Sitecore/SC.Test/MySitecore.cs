06a35121bee2c5bfc265b3446ca7a257e13410b6
Sundar Urs <sundarkurs@gmail.com>
2019-09-12 07:30:45 +0530

    Added console to access sitecore items

---
+++ Bubble/Sitecore/SC.Test/MySitecore.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Globalization;
+using Sitecore.SecurityModel;
+
+namespace SC.Test
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySitecore a = new MySitecore();
+            a.GetItem();
+        }
+    }
+
+
+    public class MySitecore
+    {
+        public void GetItem()
+        {
+            Context.SetLanguage(Language.Parse("en"), false);
+            Sitecore.Context.SetActiveSite("CustomerPortalRedesign_Au");
+
+            var db = Sitecore.Configuration.Factory.GetDatabase("master");
+            var homeItem = db.GetItem("/sitecore/content/home");
+        }
+    }
+
+
+}
